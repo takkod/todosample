25e5c8533ed90a2787f9febb400f6163c9f7af63
takkod <99372499+takkod@users.noreply.github.com>
2023-07-06 14:27:41 +0900

    v1

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import jquery from "jquery"
+window.$ = jquery
+import * as bootstrap from "bootstrap"
+//= require jquery
+//= require bootstrap-sprockets 
+
+
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "tasks#index"
+  resources :tasks
+
+
+end
